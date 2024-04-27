a3942b3cb70cf0d27a702cc219c7e53c54bb9464
Marshall <mellisgibbs@gmail.com>
2024-04-26 20:55:40 -0400

    Added pseudocode

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -2,6 +2,33 @@ package com.ps;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Store Home Screen
+            // Option 1: Display Products
+            // Option 2: Display Cart
+            // Option 3: Exit
+
+            // Display Products Screen
+                // Display items in store
+                // Option 1: Search or Filter
+                // Option 2: Add to cart
+                // Option 3: Go back
+
+            // Display Cart Screen
+                // Display items in user's cart
+                // Display total price of items in cart
+                // Option 1: Check Out cart
+                // Option 2: Remove product from cart
+                    // Specify which product to remove
+                // Option 3: Go back
+
+
+
+
+
     }
+
+    // Display Products Screen STATIC method
+
+    // Display Cart Screen STATIC method
+
 }
