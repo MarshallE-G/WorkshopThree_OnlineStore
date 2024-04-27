ddf3be0a4ee2f3618fc816481e26beb1b42b97c9
Ceyhun Cafer <ceyhuncafer1@gmail.com>
2024-04-27 15:46:01 -0400

    added add,remove,display methods for inventory class

--- src/main/java/com/ps/Inventory.java
+++ src/main/java/com/ps/Inventory.java
@@ -15,6 +15,42 @@ public class Inventory {
         searcher = new HashMap<>();
     }
 
+    // This Inventory.java class is expecting an instance of the Product.java class that we will call "product"
+    // If this is confusing, we can do Product newProduct instead.
+    // I added the basic add and remove elements that an ArrayList can do that should work for the corresponding product.
+    // Also added a basic display with sout and for loop to iterate over all products even after being added or removed from the ArrayList inventory.
+    //As for the rest of the methods we still need to add:
+    /*
+    searchByPrice()
+    searchByDepartment()
+    searchByName()
+
+     */
+
+    public void addProduct(Product product){
+       inventory.add(product);
+    }
+
+    public void displayAllProducts(){
+        for(Product product: inventory){
+            System.out.println(product);
+        }
+    }
+
+    public void displayShoppingCart(){
+        for(Product product: shoppingCart){ // Prints whatever is in the shoppingCart ArrayList
+            System.out.println(product);
+        }
+    }
+
+    public void addToShoppingCart(Product product){
+        shoppingCart.add(product);
+    }
+
+    public void removeFromShoppingCart(Product product){
+        shoppingCart.remove(product);
+    }
+
     //buffered reader will add values to instances of the above data structures.
     /*methods that print invetory, shopping cart and searching here will be called from
     the main class */
